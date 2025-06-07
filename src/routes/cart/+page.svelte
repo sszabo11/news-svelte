1b4f0ddb503a8553e7490689e60f34718209b2bc
Rabbit muncher <sszabo1082@gmail.com>
2025-06-07 19:06:50 +1000

    cart

---
+++ src/routes/cart/+page.svelte
@@ -0,0 +1,20 @@
+<div class="cart">
+	<h2>Your cart</h2>
+	<div class="item">
+		<h3>Banana</h3>
+		<h3>1 X</h3>
+		<img
+			src="https://www.shutterstock.com/image-photo/bananas-fruits-that-come-herbaceous-600nw-2552818301.jpg"
+			width={200}
+		/>
+		<h2>$59.99</h2>
+	</div>
+</div>
+
+<style>
+	.item {
+		display: flex;
+		align-items: center;
+		gap: 40px;
+	}
+</style>
